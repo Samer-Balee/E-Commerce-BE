3e5be51bcecfaa58a32ff0f0cd7d5ec57feb32f7
Samer Balee <samerbalee@gmail.com>
2022-03-06 09:34:19 +1100

    added code to get product by id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,7 +6,6 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
       attributes: ["id", "product_name", "price", "stock", "category_id"],
@@ -30,9 +29,33 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productDataById = await Product.findOne({
+      where: { id: req.params.id },
+      include: [
+        {
+          model: Category,
+          attributes: ["id", "category_name"],
+        },
+        {
+          model: Tag,
+          through: ProductTag,
+          as: "tags",
+        },
+      ],
+    })
+    if (!productDataById) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+    res.status(200).json(productDataById);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
