cb9e6c3332a45b01932fd895a7b8b5e7399e94ff
Samer Balee <samerbalee@gmail.com>
2022-03-06 00:21:38 +1100

    added code router.get to get all products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,29 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      attributes: ["id", "product_name", "price", "stock", "category_id"],
+      include: [
+        {
+          model: Category,
+          attributes: ["id", "category_name"]
+        },
+        {
+          model: Tag,
+          through: ProductTag,
+          as: "tags",
+        },
+      ],
+
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
